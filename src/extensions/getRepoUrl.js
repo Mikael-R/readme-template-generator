69205274aca22d0150520c9e01be0d242eee7a86
Mikael-R <mikarg9@gmail.com>
2020-06-22 19:08:33 -0300

    fix: 🐛 Fixed extension "getRepoUrl" trim and return undefined

--- src/extensions/getRepoUrl.js
+++ src/extensions/getRepoUrl.js
@@ -2,12 +2,12 @@ module.exports = toolbox => {
   const { filesystem: { read } } = toolbox
 
   const getRepoUrl = async () => {
-    const git_file = await read('.git/logs/refs/remotes/origin/HEAD').trim()
+    const git_file = await read('.git/logs/refs/remotes/origin/HEAD')
 
     const github_repository_url =
       git_file
-        ? git_file.split(' ')[git_file.split(' ').length - 1]
-        : undefined
+        ? git_file.split(' ')[git_file.split(' ').length - 1].trim()
+        : ''
 
     return github_repository_url
   }
